eaa506b8c324d4aeb93965f0cee2742486a7e25a
Philip Lambok <philiplambok71@gmail.com>
2022-04-23 17:48:36 +0700

    Reordering stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
     if current_user.present?
-      @requests = current_user.user_requests
+      @requests = current_user.user_requests.order(created_at: :desc)
       return false
     end
 
     user = User.create!(uuid: SecureRandom.uuid)
     cookies[:user_id] = user.uuid
-    @requests = user.user_requests
+    @requests = user.user_requests.order(created_at: :desc)
   end
 end
