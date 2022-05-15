9636bcaad3657e16a2c2b90da00ec866cbc803cb
Philip Lambok <philiplambok71@gmail.com>
2022-05-15 15:33:57 +0700

    Improve the behaviour of clear-history-btn

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -2,6 +2,7 @@ class UserRequest < ApplicationRecord
   belongs_to :user
 
   after_create_commit do |request|
-    broadcast_prepend_later_to request.user, 'user_request'
+    broadcast_prepend_to request.user, 'user_request'
+    broadcast_replace_to request.user, 'user_request', target: 'clear-history-btn', partial: 'home/clear_history_btn', locals: { current_user: request.user }
   end
 end
